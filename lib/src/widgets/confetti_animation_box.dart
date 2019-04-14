4cfbbcbcadf9979b80bfde8327ab9cfdef8d1c42
thegoobs <guthrieschoolar95@gmail.com>
2019-04-14 13:13:17 -0500

    restructured package lib/ folder to account for multiple classes exported

---
+++ lib/src/widgets/confetti_animation_box.dart
@@ -0,0 +1,64 @@
+//packages
+import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
+
+class ConfettiAnimationBox extends StatefulWidget {
+  ConfettiAnimationBox({Key key, this.child, this.duration = 5, this.amount = 30}) : super(key: key);
+
+  final Widget child;
+  final dynamic duration;
+  final int amount;
+
+  @override
+  _ConfettiAnimationBoxState createState() => _ConfettiAnimationBoxState();
+}
+
+class _ConfettiAnimationBoxState extends State<ConfettiAnimationBox> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: widget.duration),
+      vsync: this 
+    );
+
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addListener(() {
+        setState(() {
+
+        }); 
+      });
+
+    controller.forward();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> _stackList = [];
+
+    //put wrapped child at bottom of stack
+    _stackList.add(widget.child);
+
+    //add particles equal to the amount specified in amount.widget
+    for (int i = 0; i < widget.amount; i++) {
+      _stackList.add(
+        Positioned(
+          left: ((i + 1) * 10 + animation.value).toDouble(),
+          child: Container(
+            color: Colors.red,
+            width: 5,
+            height: 5,
+          ),
+        ));
+    }
+
+    return Stack(
+      children: _stackList,
+    );
+  }
+}
